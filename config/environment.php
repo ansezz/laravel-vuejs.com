efa289b4ec5bb1319a5c7f48cd04c0cf7b627e1a
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2018-07-21 14:07:00 +0100

    :tada: Initial commit.

---
+++ config/environment.php
@@ -0,0 +1,9 @@
+<?php
+
+/*----------------------------------------------------*/
+// Define environment type
+/*----------------------------------------------------*/
+return [
+    'local' => 'INSERT-HOSTNAME',
+    'production' => 'INSERT-HOSTNAME'
+];
