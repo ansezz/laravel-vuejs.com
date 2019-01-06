51f737289eb771c6d09905dcea76f908e90ea082
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-01-06 15:17:38 +0100

    add media image  seeder

--- back/database/seeds/UsersTableSeeder.php
+++ back/database/seeds/UsersTableSeeder.php
@@ -1,15 +1,17 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
 class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      *
+     * @param \Faker\Generator $faker
+     *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker): void
     {
 
         $admin = new \LaravelVueJs\Models\User([
@@ -19,8 +21,19 @@ class UsersTableSeeder extends Seeder
         ]);
         $admin->save();
 
-        factory(\LaravelVueJs\Models\User::class, 50)->create()->each(function (\LaravelVueJs\Models\User $user) {
-            $user->posts()->save(factory(\LaravelVueJs\Models\Post::class)->make());
+        factory(\LaravelVueJs\Models\User::class, 10)->create()->each(function (\LaravelVueJs\Models\User $user) use (
+            $faker
+        ) {
+            factory(\LaravelVueJs\Models\Post::class, 2)->make()->each(function (\LaravelVueJs\Models\Post $post) use (
+                $user,
+                $faker
+            ) {
+                $post->user_id = $user->id;
+                $post->addMediaFromUrl($faker->imageUrl())
+                    ->toMediaCollection(\LaravelVueJs\Models\Post::MEDIA_COLLECTION);
+                $post->save();
+            });
+
         });
     }
 }
