91914b6926fecc4ee29ba966021740ae7c34d130
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-01-07 00:57:18 +0100

    deploy fix hureko

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -24,42 +24,12 @@ module.exports = {
   ],
   // Give apollo module options
   apollo: {
-    tokenName: 'yourApolloTokenName', // optional, default: apollo-token
-    tokenExpires: 10, // optional, default: 7 (days)
-    includeNodeModules: true, // optional, default: false (this includes graphql-tag for node_modules folder)
-    authenticationType: 'Basic', // optional, default: 'Bearer'
-    // optional
-    errorHandler(error) {
-      console.log('%cError', 'background: red; color: white; padding: 2px 4px; border-radius: 3px; font-weight: bold;', error.message)
-    },
     // required
     clientConfigs: {
       default: {
-        // required
-        httpEndpoint: process.env.GRAPHQL_URL,//'http://local.laravel-vuejs.com/graphql',
-        // optional
-        // See https://www.apollographql.com/docs/link/links/http.html#options
-        httpLinkOptions: {
-          credentials: 'same-origin'
-        },
-        // You can use `wss` for secure connection (recommended in production)
-        // Use `null` to disable subscriptions
-        wsEndpoint: 'ws://localhost:4000', // optional
-        // LocalStorage token
-        tokenName: 'apollo-token', // optional
-        // Enable Automatic Query persisting with Apollo Engine
-        persisting: false, // Optional
-        // Use websockets for everything (no HTTP)
-        // You need to pass a `wsEndpoint` for this to work
-        websocketsOnly: false // Optional
-      },
-      test: {
-        httpEndpoint: 'http://localhost:5000',
-        wsEndpoint: 'ws://localhost:5000',
-        tokenName: 'apollo-token'
-      },
-      // alternative: user path to config which returns exact same config options
-      // test2: '~/plugins/my-alternative-apollo-config.js'
+        httpEndpoint: process.env.GRAPHQL_URL,
+        wsEndpoint: process.env.GRAPHQL_WS
+      }
     }
   },
   /* sitemap: {
