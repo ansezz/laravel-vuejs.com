31059d02a9c5ef5bed6040c35cb5378d93b1dfc0
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-01-06 15:35:23 +0100

    update env conf

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -5,8 +5,8 @@ const axios = require("axios")
 const resolve = require("path").resolve
 
 module.exports = {
-    modules: [
-      '@nuxtjs/apollo',
+  modules: [
+    '@nuxtjs/apollo',
     "@nuxtjs/axios",
     "@nuxtjs/dotenv",
     "@nuxtjs/font-awesome",
@@ -36,7 +36,7 @@ module.exports = {
     clientConfigs: {
       default: {
         // required
-        httpEndpoint: 'http://local.laravel-vuejs.com/graphql',
+        httpEndpoint: process.env.GRAPHQL_URL,//'http://local.laravel-vuejs.com/graphql',
         // optional
         // See https://www.apollographql.com/docs/link/links/http.html#options
         httpLinkOptions: {
@@ -62,147 +62,148 @@ module.exports = {
       // test2: '~/plugins/my-alternative-apollo-config.js'
     }
   },
-    sitemap: {
-        path: "/sitemap.xml",
-        // hostname: 'https://example.com',
-        cacheTime: 1000 * 60 * 15,
-        gzip: true,
-        generate: false, // Enable me when using nuxt generate
-        exclude: [
+  sitemap: {
+    path: "/sitemap.xml",
+    // hostname: 'https://example.com',
+    cacheTime: 1000 * 60 * 15,
+    gzip: true,
+    generate: false, // Enable me when using nuxt generate
+    exclude: [
       // '/cms/**'
     ],
-        routes() {
-            return axios
-                .get(process.env.SITE_MAP_URL)
-                .then(res => res.data.data.data.map(post => "/" + post.slug))
-        }
-    },
-    /*"google-analytics": {
-        id: process.env.GOOGLE_ANALYTICS
-    },*/
-    axios: {
-        baseURL: process.env.API_URL,
-        headers: {
-            "X-Requested-With": "XMLHttpRequest"
-        },
-        withCredentials: false
-    },
-    manifest: {
-        name: "Nuxt Demo",
-        short_name: "NuxtDemo",
-        title: "Nuxt Demo Title",
-        "og:title": "Nuxt Demo ogTitle",
-        description: "Nuxt Demo  appDescription",
-        "og:description": "Nuxt Demo  ogDescription",
-        lang: "en",
-        theme_color: "#42b883",
-        background_color: "#35495e"
-    },
-    router: {
-        middleware: "platform"
-    },
-    /*
-     ** Headers of the page
-     */
-    head: {
-        title: "Nuxt Demo",
-        meta: [
-            {
-                charset: "utf-8"
-            },
-            {
-                name: "viewport",
-                content: "width=device-width, initial-scale=1"
-            },
-            {
-                hid: "description",
-                name: "description",
-                content: "Nuxt.js project"
-            }
-    ],
-        htmlAttrs: {
-            dir: "ltr"
-        },
-        link: [
-            {
-                rel: "icon",
-                type: "image/x-icon",
-                href: "/favicon.ico"
-            },
-    ],
-        script: []
-    },
-    /*
-     ** Customize the progress bar color
-     */
-    loading: {
-        color: "#63F9E6"
-    },
-    // @TODO : custom loading
-    //  loading: '~/components/loading.vue'
-
-    /*
-     ** Build configuration
-     */
-    build: {
-        plugins: [
-            new webpack.LoaderOptionsPlugin({
-                options: {
-                    stylus: {
-                        import: [
-                            resolve(__dirname, "./assets/stylus/index.styl"),
-                            "~rupture/rupture/index.styl"
-                        ]
-                    }
-                }
-            })
-        ],
-        // vendor: ['vue-i18n'],
-        postcss: [require("postcss-rtl")(), require("lost")()],
-        extend(config, {
-            isDev,
-            isClient
-        }) {}
-    },
-
-    //
-    plugins: [
-        {
-            src: "~/plugins/ui"
-        },
-        {
-            src: "~/plugins/http"
-        },
-        {
-            src: "~/plugins/social-sharing"
-        },
-        {
-            src: "~/plugins/swiper",
-            ssr: false
-        },
-        {
-            src: "~/plugins/lazyload",
-            ssr: false
-        },
-        {
-            src: '~/plugins/vue-tags-input',
-            ssr: false
-        }
-    ],
-
-    css: [
-        "swiper/dist/css/swiper.css",
-        "ionicons/dist/css/ionicons.min.css",
-        "bootstrap/dist/css/bootstrap.css",
-        "~/assets/AvenirNextLTPro.css"
-    ],
-
-    // yarn nuxt build --webpackmonitor --analyze
-    webpackMonitor: {
-        capture: true, // -> default 'true'
-        // target: '../monitor/myStatsStore.json', // default -> '../monitor/stats.json'
-        launch: true, // -> default 'false'
-        port: 3030, // default -> 8081
-        excludeSourceMaps: true // default 'true'
+    routes() {
+      return axios
+        .get(process.env.SITE_MAP_URL)
+        .then(res => res.data.data.data.map(post => "/" + post.slug))
     }
+  },
+  /*"google-analytics": {
+      id: process.env.GOOGLE_ANALYTICS
+  },*/
+  axios: {
+    baseURL: process.env.API_URL,
+    headers: {
+      "X-Requested-With": "XMLHttpRequest"
+    },
+    withCredentials: false
+  },
+  manifest: {
+    name: "Nuxt Demo",
+    short_name: "NuxtDemo",
+    title: "Nuxt Demo Title",
+    "og:title": "Nuxt Demo ogTitle",
+    description: "Nuxt Demo  appDescription",
+    "og:description": "Nuxt Demo  ogDescription",
+    lang: "en",
+    theme_color: "#42b883",
+    background_color: "#35495e"
+  },
+  router: {
+    middleware: "platform"
+  },
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: "Nuxt Demo",
+    meta: [
+      {
+        charset: "utf-8"
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: "Nuxt.js project"
+      }
+    ],
+    htmlAttrs: {
+      dir: "ltr"
+    },
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico"
+      },
+    ],
+    script: []
+  },
+  /*
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: "#63F9E6"
+  },
+  // @TODO : custom loading
+  //  loading: '~/components/loading.vue'
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    plugins: [
+      new webpack.LoaderOptionsPlugin({
+        options: {
+          stylus: {
+            import: [
+              resolve(__dirname, "./assets/stylus/index.styl"),
+              "~rupture/rupture/index.styl"
+            ]
+          }
+        }
+      })
+    ],
+    // vendor: ['vue-i18n'],
+    postcss: [require("postcss-rtl")(), require("lost")()],
+    extend(config, {
+      isDev,
+      isClient
+    }) {
+    }
+  },
+
+  //
+  plugins: [
+    {
+      src: "~/plugins/ui"
+    },
+    {
+      src: "~/plugins/http"
+    },
+    {
+      src: "~/plugins/social-sharing"
+    },
+    {
+      src: "~/plugins/swiper",
+      ssr: false
+    },
+    {
+      src: "~/plugins/lazyload",
+      ssr: false
+    },
+    {
+      src: '~/plugins/vue-tags-input',
+      ssr: false
+    }
+  ],
+
+  css: [
+    "swiper/dist/css/swiper.css",
+    "ionicons/dist/css/ionicons.min.css",
+    "bootstrap/dist/css/bootstrap.css",
+    "~/assets/AvenirNextLTPro.css"
+  ],
+
+  // yarn nuxt build --webpackmonitor --analyze
+  webpackMonitor: {
+    capture: true, // -> default 'true'
+    // target: '../monitor/myStatsStore.json', // default -> '../monitor/stats.json'
+    launch: true, // -> default 'false'
+    port: 3030, // default -> 8081
+    excludeSourceMaps: true // default 'true'
+  }
 }
