0117d26dfe88251f5f064761baeaaf8ce9e13934
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-01-07 01:05:18 +0100

    deploy fix hureko

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -4,8 +4,6 @@ require("dotenv").config()
 // const axios = require("axios")
 const resolve = require("path").resolve
 
-console.log(process.env)
-
 module.exports = {
   modules: [
     '@nuxtjs/apollo',
