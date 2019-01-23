0702bf33815daf16b61f6de8f5f15fe1239aaac1
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-01-23 13:30:49 +0100

    add views counter with event

--- back/app/Providers/EventServiceProvider.php
+++ back/app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use LaravelVueJs\Events\PostViewed;
+use LaravelVueJs\Listeners\PostViewsIncrement;
 use LaravelVueJs\Listeners\SendWelcomeEmailNotification;
 
 
@@ -18,8 +20,11 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         Registered::class => [
-          //  SendEmailVerificationNotification::class,
+            //  SendEmailVerificationNotification::class,
             SendWelcomeEmailNotification::class,
+        ],
+        PostViewed::class => [
+            PostViewsIncrement::class,
         ],
     ];
 
