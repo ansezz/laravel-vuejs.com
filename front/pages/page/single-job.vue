a702501a00491362e1efa42d289c9655af92162b
Othman <othmandeveloppeur@gmail.com>
2019-01-28 11:14:44 +0100

    Single Job Page & Add Assets

---
+++ front/pages/page/single-job.vue
@@ -0,0 +1,16 @@
+<template>
+  <component :is="this.$store.state.platform"/>
+</template>
+
+<script>
+  export default {
+    layout: ({store}) => store.state.platform,
+    components: {
+      mobile: () => import(`~/views/mobile/page/single-job`),
+      web: () => import(`~/views/web/page/single-job`)
+    }
+  }
+</script>
+
+<style>
+</style>
