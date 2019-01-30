77f66ac10c97eaef093f131cfd0d9b9422e89e95
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-01-30 20:23:07 +0100

    fix load posts home

--- front/middleware/home.js
+++ front/middleware/home.js
@@ -1,4 +1,8 @@
 export default function (context) {
-  context.store.dispatch("post/LOAD_POSTS")
+  context.store.dispatch("post/LOAD_POSTS", {
+    count: context.query.count,
+    page: context.query.page,
+    sort_by: context.query.sort_by,
+  })
   return context.store.dispatch("post/LOAD_FEATURED_POSTS")
 }
