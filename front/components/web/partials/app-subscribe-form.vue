37efc93962924462bc4577aca03f05832daadb93
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-01-31 01:32:55 +0100

    subscribe user by email

--- front/components/web/partials/app-subscribe-form.vue
+++ front/components/web/partials/app-subscribe-form.vue
@@ -1,43 +1,72 @@
 <template>
-    <div class="widget">
-        <heading>Subsribe</heading>
-        <app-form class="subscribe-widget">
-           <template slot="desc">
-                <p>Sign up for our newsletter, and we'll send you news and tutorials on web design, coding, business, and more!</p>
-            </template>
-            <div class="form-group">
-                <input type="email" class="form-control" placeholder="E-mail">
-                <div class="img">
-                    <img src="@/assets/images/icons-email.png" alt="#">
-                </div>
-            </div>
-            <template slot="actions">
-                <button class="button" type="submit">Subscribe</button>
-            </template>
-        </app-form>
-    </div>
+  <div class="widget">
+    <heading>Subsribe</heading>
+    <app-form class="subscribe-widget">
+      <template slot="desc">
+        <p>Sign up for our newsletter, and we'll send you news and tutorials on web design, coding, business, and
+          more!</p>
+      </template>
+      <div class="alert alert-success" role="alert" v-if="message">
+        {{message}}
+      </div>
+      <div class="form-group">
+        <input type="email" class="form-control" placeholder="E-mail" v-model="email">
+        <div class="img">
+          <img src="@/assets/images/icons-email.png" alt="#">
+        </div>
+      </div>
+      <template slot="actions">
+        <button class="button" type="submit" @click.prevent="subscribe">Subscribe</button>
+      </template>
+    </app-form>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'AppSubscribeForm'
-    }
+  import subscribeQql from '@/graphql/mutations/subscribe.graphql';
+
+  export default {
+    name: 'AppSubscribeForm',
+    data() {
+      return {
+        email: null,
+        message: null
+      }
+    },
+    methods: {
+      subscribe() {
+        if (!this.email)
+          return;
+
+        let variables = {email: this.email};
+        this.message = null;
+        this.$apollo.mutate({mutation: subscribeQql, variables})
+          .then((data) => {
+            this.message = data.data.subscribe
+          })
+      }
+    },
+  }
 </script>
 
 <style lang="stylus" scoped>
-    .heading
-        margin-bottom 30px
-    .widget
-        margin-bottom 63px
-        &.last-widget
-            margin-bottom 0
-    .subscribe-widget
-        p
-            font-size 14px
-            font-weight 400
-            color $tertiary
-            margin-bottom 20px
-            opacity .8
-        .form-control
-            background-color rgba($secondary, .03)
+  .heading
+    margin-bottom 30px
+
+  .widget
+    margin-bottom 63px
+
+    &.last-widget
+      margin-bottom 0
+
+  .subscribe-widget
+    p
+      font-size 14px
+      font-weight 400
+      color $tertiary
+      margin-bottom 20px
+      opacity .8
+
+    .form-control
+      background-color rgba($secondary, .03)
 </style>
