0d049ac8463eb7f12f2d7a60f9078bc3ba037eb2
Othman <othmandeveloppeur@gmail.com>
2019-03-14 21:39:30 +0100

    show sign up form if not loggin

--- front/components/web/partials/app-header.vue
+++ front/components/web/partials/app-header.vue
@@ -2,14 +2,15 @@
     <section class="app-header">
         <div class="container">
             <div class="app-header-flexed">
-                <app-articles-swiper />
-                <app-sign-up-form />
+                <app-articles-swiper :class="loggedIn ? 'full-width' : '' "/>
+                <app-sign-up-form v-if="!loggedIn" />
             </div>
         </div>
     </section>
 </template>
 
 <script>
+    import {mapActions} from 'vuex'
     import AppArticlesSwiper from "@/components/web/partials/app-articles-swiper"
     import AppSignUpForm from "@/components/web/partials/app-sign-up-form"
     export default {
@@ -20,7 +21,7 @@
         },
         data() {
             return {
-                
+              loggedIn: this.$store.state.auth.loggedIn
             }
         }
     }
@@ -28,10 +29,16 @@
 </script>
 
 <style lang="stylus" scoped>
-    .app-header
-        background-color $tertiary
-        padding 60px 0
-        .app-header-flexed
-            display flex
-            justify-content space-between
+  .app-header
+      background-color $tertiary
+      padding 60px 0
+      .app-header-flexed
+          display flex
+          justify-content space-between
+  .articles-swiper
+    &.full-width
+      width 100%
+      & >>>
+        .swiper-wrapper
+          width calc(100% - 35px)
 </style>
