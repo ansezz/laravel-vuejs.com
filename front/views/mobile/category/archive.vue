22c70bb75580d381f17d47cd181d4c27ce636dd7
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-03-16 16:03:47 +0100

    mobile archive the same as desktop need adapt for mobile

--- front/views/mobile/category/archive.vue
+++ front/views/mobile/category/archive.vue
@@ -1,15 +1,188 @@
-<template lang="html">
-  <div>
-    posts by category mobile
-  </div>
+<!--waiting designer-->
+<template>
+    <section class="posts-container">
+        <breadcrumb :pages="breadcrumbsData()"/>
+        <div class="container">
+            <div class="post-heading-filters">
+                <div class="post-heading">
+                    <div class="user-avatar">
+                        <img src="@/assets/images/icons-category.svg" alt="LV">
+                    </div>
+                    <h1 v-if="category">{{category.name}}</h1>
+                </div>
+                <filters :filter.sync="filter" :action.sync="filterChange"></filters>
+            </div>
+            <div class="article-grid">
+                <article-item v-for="item in postsByCategory.data"
+                              :title="item.title"
+                              :image="item.image_url"
+                              :description="item.excerpt"
+                              :key="item.id"
+                              :to="{ name: 'slug', params: { slug: item.slug }}"
+                />
+            </div>
+
+            <div class="adsbygoogle">
+                <adsbygoogle/>
+            </div>
+
+            <button @click="showMore()"
+                    v-if="hasMorePages && !show_more">
+                {{ $apollo.queries.postsByCategory.loading ? 'Loading ...' : 'Show more'}}
+            </button>
+
+            <no-ssr>
+                <infinite-loading
+                        @infinite="showMore"
+                        v-if="show_more"></infinite-loading>
+            </no-ssr>
+        </div>
+    </section>
 </template>
 
 <script>
-  export default {
-    components: {},
-    name: "posts-category"
-  }
+
+    import postsQql from '@/graphql/queries/category/posts.graphql';
+    import InfiniteLoading from 'vue-infinite-loading';
+
+    export default {
+        components: {
+            filters: () => import('@/components/shared/partials/elements/filters'),
+            Breadcrumb: () => import('@/components/shared/partials/elements/breadcrumb'),
+            ArticleItem: () => import('@/components/shared/partials/elements/article-item'),
+            InfiniteLoading
+        },
+        name: "category-posts",
+        apollo: {
+            postsByCategory: {
+                query: postsQql,
+                variables() {
+                    // Initial variables
+                    return {
+                        count: 12,
+                        page: 1,
+                        sort_by: 'latest',
+                        slug: this.$route.params.slug,
+                    }
+                }
+            },
+        },
+        computed: {
+            category() {
+                return this.$store.state.category.category
+            },
+            hasMorePages() {
+                return this.postsByCategory && this.postsByCategory.paginatorInfo && this.postsByCategory.paginatorInfo.hasMorePages;
+            }
+        },
+        methods: {
+            showMore($state) {
+                if (!this.hasMorePages || this.$apollo.queries.postsByCategory.loading) {
+                    return true
+                }
+
+                this.filter.page++
+
+                this.$apollo.queries.postsByCategory.fetchMore({
+                    // New variables
+                    variables: {
+                        count: this.filter.count,
+                        page: this.filter.page,
+                        sort_by: this.filter.sort_by,
+                        slug: this.$route.params.slug
+                    },
+                    // Transform the previous result with new data
+                    updateQuery: (previousResult, {fetchMoreResult}) => {
+                        if (!fetchMoreResult)
+                            return previousResult;
+
+                        if ($state) {
+                            if (fetchMoreResult.postsByCategory.paginatorInfo.hasMorePages)
+                                $state.loaded();
+                            else
+                                $state.complete();
+                        }
+
+                        this.show_more = true;
+                        fetchMoreResult.postsByCategory.data = [...previousResult.postsByCategory.data, ...fetchMoreResult.postsByCategory.data];
+                        return fetchMoreResult;
+                    },
+                })
+            },
+            filterChange() {
+                this.$router.push({
+                    name: 'category-slug',
+                    query: {count: this.filter.count, sort_by: this.filter.sort_by},
+                    params: {slug: this.$route.params.slug}
+                })
+            }
+        },
+        data() {
+            return {
+                postsByCategory: {},
+                show_more: false,
+                filter: {
+                    page: 1,
+                    sort_by: 'latest',
+                    count: 12,
+                },
+                breadcrumbsData: () => [
+                    {
+                        name: 'Home',
+                        link: "/"
+                    },
+                    {
+                        name: 'Category'
+                    },
+                    {
+                        name: this.category.name
+                    }
+                ]
+            }
+        }
+    }
+
 </script>
 
-<style>
+<style lang="stylus" scoped>
+    .posts-container
+        padding-bottom 120px
+
+    .post-heading-filters
+        position relative
+        height 60px
+        overflow hidden
+        display flex
+        align-items flex-end
+        justify-content flex-end
+        margin 60px 0 40px
+
+    .post-heading
+        position absolute
+        left 50%
+        transform translateX(-50%)
+        text-align center
+
+        h1
+            font-size 28px
+            font-weight 600
+            color $tertiary
+            margin-top 10px
+            line-height 1
+
+    .article-grid
+        display grid
+        grid-template-columns repeat(4, 1fr)
+        grid-gap 40px 10px
+
+    .ads
+        width 900px
+        height 250px
+        display flex
+        align-items center
+        justify-content center
+        background-color #3abbff
+        font-size 24px
+        color $white
+        margin 60px auto
 </style>
--- front/views/mobile/tag/archive.vue
+++ front/views/mobile/tag/archive.vue
@@ -1,15 +1,215 @@
-<template lang="html">
-  <div>
-    posts by tag mobile
-  </div>
+<!--waiting designer-->
+<template>
+    <section class="posts-container">
+        <breadcrumb :pages="breadcrumbsData()"/>
+        <div class="container">
+            <div class="post-heading-filters">
+                <div class="post-heading">
+                    <div class="user-avatar">
+                        <img src="@/assets/images/icons-category.svg" alt="LV">
+                    </div>
+                    <h1 v-if="tag">{{tag.name}}</h1>
+                </div>
+                <filters :filter.sync="filter" :action.sync="filterChange"></filters>
+            </div>
+            <div class="article-grid">
+                <article-item v-for="item in postsByTag.data"
+                              :title="item.title"
+                              :image="item.image_url"
+                              :description="item.excerpt"
+                              :key="item.id"
+                              :to="{ name: 'slug', params: { slug: item.slug }}"
+                />
+            </div>
+            <div class="adsbygoogle">
+                <adsbygoogle/>
+            </div>
+
+            <button @click="showMore()"
+                    v-if="hasMorePages && !show_more">
+                {{ $apollo.queries.postsByTag.loading ? 'Loading ...' : 'Show more'}}
+            </button>
+
+            <no-ssr>
+                <infinite-loading
+                        @infinite="showMore"
+                        v-if="show_more"></infinite-loading>
+            </no-ssr>
+        </div>
+    </section>
 </template>
 
 <script>
-  export default {
-    components: {},
-    name: "posts-tag"
-  }
+    import postsQql from '@/graphql/queries/tag/posts.graphql';
+    import InfiniteLoading from 'vue-infinite-loading';
+
+    export default {
+        components: {
+            filters: () => import('@/components/shared/partials/elements/filters'),
+            Breadcrumb: () => import('@/components/shared/partials/elements/breadcrumb'),
+            ArticleItem: () => import('@/components/shared/partials/elements/article-item'),
+            InfiniteLoading
+        },
+        name: "tag-posts",
+        apollo: {
+            postsByTag: {
+                query: postsQql,
+                variables() {
+                    // Initial variables
+                    return {
+                        count: 12,
+                        page: 1,
+                        sort_by: 'latest',
+                        slug: this.$route.params.slug,
+                    }
+                }
+            },
+        },
+        computed: {
+            tag() {
+                return this.$store.state.tag.tag
+            },
+            hasMorePages() {
+                return this.postsByTag && this.postsByTag.paginatorInfo && this.postsByTag.paginatorInfo.hasMorePages;
+            }
+        },
+        methods: {
+            showMore($state) {
+                if (!this.hasMorePages || this.$apollo.queries.postsByTag.loading) {
+                    return true
+                }
+
+                this.filter.page++
+
+                this.$apollo.queries.postsByTag.fetchMore({
+                    // New variables
+                    variables: {
+                        count: this.filter.count,
+                        page: this.filter.page,
+                        sort_by: this.filter.sort_by,
+                        slug: this.$route.params.slug
+                    },
+                    // Transform the previous result with new data
+                    updateQuery: (previousResult, {fetchMoreResult}) => {
+                        if (!fetchMoreResult)
+                            return previousResult;
+
+                        if ($state) {
+                            if (fetchMoreResult.postsByTag.paginatorInfo.hasMorePages)
+                                $state.loaded();
+                            else
+                                $state.complete();
+                        }
+
+                        this.show_more = true;
+                        fetchMoreResult.postsByTag.data = [...previousResult.postsByTag.data, ...fetchMoreResult.postsByTag.data];
+                        return fetchMoreResult;
+                    },
+                })
+            },
+            filterChange() {
+                this.$router.push({
+                    name: 'tag-slug',
+                    query: {count: this.filter.count, sort_by: this.filter.sort_by},
+                    params: {slug: this.$route.params.slug}
+                })
+            }
+        },
+        data() {
+            return {
+                postsByTag: {},
+                show_more: false,
+                filter: {
+                    page: 1,
+                    sort_by: 'latest',
+                    count: 12,
+                },
+                breadcrumbsData: () => [{
+                    name: 'Home',
+                    link: "/"
+                },
+                    {
+                        name: 'Tag'
+                    },
+                    {
+                        name: this.tag.name
+                    }
+                ]
+            }
+        }
+    }
+
 </script>
 
-<style>
+<style lang="stylus" scoped>
+    .posts-container
+        padding-bottom 120px
+
+    .post-heading-filters
+        position relative
+        height 60px
+        overflow hidden
+        display flex
+        align-items flex-end
+        justify-content flex-end
+        margin 60px 0 40px
+
+    .post-heading
+        position absolute
+        left 50%
+        transform translateX(-50%)
+        text-align center
+
+        h1
+            font-size 28px
+            font-weight 600
+            color $tertiary
+            margin-top 10px
+            line-height 1
+
+    .filters
+        display flex
+        align-items center
+
+        .filter-group
+            position relative
+            margin-right 45px
+
+            &:after
+                content "\f0d7"
+                font-family "FontAwesome"
+                color $secondary
+                padding-left 15px
+
+            &:last-child
+                margin-right 0
+
+            select
+                font-size 12px
+                font-weight 600
+                letter-spacing 2px
+                color $secondary
+                text-transform uppercase
+                border 0
+                background-color transparent
+                outline none
+                -webkit-appearance none
+                -moz-appearance none
+                appearance none
+
+    .article-grid
+        display grid
+        grid-template-columns repeat(4, 1fr)
+        grid-gap 40px 10px
+
+    .ads
+        width 900px
+        height 250px
+        display flex
+        align-items center
+        justify-content center
+        background-color #3abbff
+        font-size 24px
+        color $white
+        margin 60px auto
 </style>
