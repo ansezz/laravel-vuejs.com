411941b831ef7e4cd09c9b8118fb2874daae049b
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-03-16 17:06:50 +0100

    add menus and social media links to config (i will change them by API soon )

--- front/config/index.js
+++ front/config/index.js
@@ -1,2 +1,114 @@
 export const GOOGLE_ANALYTICS = process.env.GOOGLE_ANALYTICS
 export const DEFAULT_LANG = process.env.DEFAULT_LANG
+export const main_menu = [
+    {
+        name: 'Home',
+        url: '/',
+    },
+    {
+        name: 'Laravel',
+        url: '/category/laravel',
+    },
+    {
+        name: 'VueJs',
+        url: '/category/vuejs',
+    },
+    {
+        name: 'Jobs',
+        url: '/category/jobs',
+    },
+    {
+        name: 'eBooks',
+        url: '/category/ebooks',
+    }
+]
+
+export const second_menu = [
+    {
+        name: 'Home',
+        url: '/',
+    },
+    {
+        name: 'DMCA Policy',
+        url: '/dmca-policy',
+    },
+    {
+        name: 'Privacy Policy',
+        url: '/privacy-policy',
+    },
+    {
+        name: 'Terms and Conditions',
+        url: '/terms-and-conditions',
+    },
+    {
+        name: 'Contact us',
+        url: '/contact-us',
+    },
+    {
+        name: 'About us',
+        url: '/about-us',
+    }
+]
+
+export const social_media = [
+    {
+        title: "Google+",
+        href: "https://plus.google.com/+LaravelVueJS",
+        class: "fa fa-google-plus",
+    },
+    {
+        title: "Facebook",
+        href: "https://www.facebook.com/LaravelVueJs",
+        class: "fa fa-facebook"
+    },
+    {
+        title: "Twitter",
+        href: "https://twitter.com/LaravelVueJs",
+        class: "fa fa-twitter"
+    },
+    {
+        title: "Pinterest",
+        href: "https://www.pinterest.com/laravelvuejs/",
+        class: "fa fa-pinterest"
+    },
+    {
+        title: "Flickr",
+        href: "https://www.flickr.com/photos/laravelvuejs/",
+        class: "fa fa-flickr"
+    },
+    {
+        title: "Reddit",
+        href: "https://www.reddit.com/user/laravelvuejs",
+        class: "fa fa-reddit"
+    },
+    {
+        title: "StumbleUpon",
+        href: "https://www.stumbleupon.com/stumbler/laravelvuejs",
+        class: "fa fa-stumbleupon"
+    },
+    {
+        title: "Tumblr",
+        href: "https://laravelvuejs.tumblr.com",
+        class: "fa fa-tumblr"
+    },
+    {
+        title: "Instagram",
+        href: "https://www.instagram.com/laravelvuejs/",
+        class: "fa fa-instagram"
+    },
+    {
+        title: "vk.,com",
+        href: "https://vk.com/laravelvuejs",
+        class: "fa fa-vk"
+    },
+    {
+        title: "Telegram",
+        href: "http://t.me/LaravelVueJs",
+        class: "fa fa-paper-plane"
+    },
+    {
+        title: "Rss",
+        href: "https://www.laravel-vuejs.com/feed/",
+        class: "fa fa-rss"
+    },
+]
