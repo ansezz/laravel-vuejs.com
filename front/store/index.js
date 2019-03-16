ff37c69481cb8af6b99b01cb217a51554162c598
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-03-16 17:07:06 +0100

    add menu and social links to vuex store

--- front/store/index.js
+++ front/store/index.js
@@ -1,23 +1,28 @@
+import {main_menu, second_menu, social_media} from '@/config'
+
 export const state = () => ({
-  platform: "",
-  navigation: {
-    visibility: false
-  }
+    platform: "",
+    navigation: {
+        visibility: false
+    },
+    social_media,
+    main_menu,
+    second_menu
 })
 
 export const mutations = {
-  SET_PLATFORM: (state, {platform}) => {
-    state.platform = platform
-  },
-  SET_NAVIGATION_VISIBILITY: (state, val) => {
-    state.navigation.visibility = val
-  }
+    SET_PLATFORM: (state, {platform}) => {
+        state.platform = platform
+    },
+    SET_NAVIGATION_VISIBILITY: (state, val) => {
+        state.navigation.visibility = val
+    }
 }
 
 export const getters = {}
 
 export const actions = {
-  toggleNavigationVisibility({commit, state}) {
-    commit("SET_NAVIGATION_VISIBILITY", !state.navigation.visibility)
-  }
+    toggleNavigationVisibility({commit, state}) {
+        commit("SET_NAVIGATION_VISIBILITY", !state.navigation.visibility)
+    }
 }
