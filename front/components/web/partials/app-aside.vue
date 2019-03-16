6fe16520413f5281025e11b2a9027bff2b3077dc
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-03-16 17:07:26 +0100

    add all social media

--- front/components/web/partials/app-aside.vue
+++ front/components/web/partials/app-aside.vue
@@ -5,23 +5,26 @@
                 <heading>Follow us</heading>
                 <div class="widget">
                     <ul class="social-media">
-                        <li v-for="(item, index) in ['rss', 'youtube', 'instagram', 'twitter', 'facebook']" :key="index"     :class="item">
-                            <nuxt-link to="/"><i :class="`fa fa-${item}`" /></nuxt-link>
+                        <li v-for="(item, index) in social_media" :key="index"
+                            :class="item.name">
+                            <a :href="item.href" target="_blank">
+                                <i class="fa" :class="item.class"/>
+                            </a>
                         </li>
                     </ul>
                 </div>
-                <app-popular-posts />
+                <app-popular-posts/>
                 <div class="widget">
                     <adsbygoogle/>
                 </div>
-                <app-subscribe-form />
-              <div class="widget last-widget">
-                      <adsbygoogle />
-              </div>
-              <div class="widget last-widget">
-                <div class="ads">300x250</div>
-                  <adsbygoogle/>
-              </div>
+                <app-subscribe-form/>
+                <div class="widget last-widget">
+                    <adsbygoogle/>
+                </div>
+                <div class="widget last-widget">
+                    <div class="ads">300x250</div>
+                    <adsbygoogle/>
+                </div>
             </div>
         </aside>
     </section>
@@ -30,8 +33,14 @@
 <script>
     import AppSubscribeForm from "@/components/web/partials/app-subscribe-form"
     import AppPopularPosts from "@/components/web/partials/app-popular-posts"
+
     export default {
         name: 'AppAside',
+        computed: {
+            social_media() {
+                return this.$store.state.social_media;
+            }
+        },
         components: {
             AppSubscribeForm,
             AppPopularPosts
@@ -42,38 +51,51 @@
 <style lang="stylus" scoped>
     .sidebar
         width 300px
+
         .heading
             margin-bottom 30px
+
         .widget
             margin-bottom 63px
+
             &.last-widget
                 margin-bottom 0
+
         .social-media
             display flex
             align-items center
+
             li
                 padding-right 20px
+
                 a
                     display block
                     width 16px
                     font-size 16px
+
                 &.rss
                     a
                         color #ff6f00
+
                 &.youtube
                     a
                         color #e54c4b
+
                 &.instagram
                     a
                         color #3897f0
+
                 &.twitter
                     a
                         color #1da1f2
+
                 &.facebook
                     a
                         color #3b5998
+
                 &:last-child
                     padding-right 0
+
         .ads
             width 100%
             height 250px
