885ca7b501a371c06ec1f596104cdc26cd7c95d9
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-03-18 22:43:30 +0100

    login and signup in home page

--- front/components/web/partials/app-sign-up-form.vue
+++ front/components/web/partials/app-sign-up-form.vue
@@ -2,15 +2,43 @@
     <section>
         <div class="sign-up-form" v-if="register" id="register">
             <h3>Sign Up Now!</h3>
-            <form>
+            <form @submit.prevent="submitSignup" :class="{'is-loading' : form.loading}">
                 <div class="form-group">
-                    <input type="text" name="fullname" class="form-control has-custom" placeholder="Full Name">
+                    <input type="text" class="form-control has-custom"
+                           name="name"
+                           placeholder="Name"
+                           v-model="form.name"
+                           required
+                           v-validate="'required'"
+                    >
+                    <no-ssr>
+                        <small class="text-danger" v-if="errors">{{ errors.first('name') }}</small>
+                    </no-ssr>
                 </div>
                 <div class="form-group">
-                    <input type="email" name="email" class="form-control has-custom" placeholder="E-Mail">
+                    <input type="email"
+                           class="form-control has-custom"
+                           name="email"
+                           placeholder="E-mail"
+                           v-model="form.email"
+                           required
+                           v-validate="'required|email'">
+                    <no-ssr>
+                        <small class="text-danger" v-if="errors">{{ errors.first('email') }}</small>
+                    </no-ssr>
                 </div>
                 <div class="form-group">
-                    <input type="password" name="password" class="form-control has-custom" placeholder="Password">
+                    <input type="password"
+                           class="form-control has-custom"
+                           placeholder="Password"
+                           v-model="form.password"
+                           name="password"
+                           required
+                           v-validate="'required|min:6'"
+                    >
+                    <no-ssr>
+                        <small class="text-danger" v-if="errors">{{ errors.first('password') }}</small>
+                    </no-ssr>
                 </div>
                 <ul class="form-status">
                     <li></li>
@@ -18,8 +46,13 @@
                     <li></li>
                 </ul>
                 <div class="checking">
-                    <input type="checkbox" id="checked" class="check">
+                    <input type="checkbox" id="checked" class="check" v-model="form.newsletter">
                     <label for="checked">I want to hear from you about products and services.</label>
+                </div>
+                <div class="alert alert-danger" role="alert" v-if="validation">
+                    <ul>
+                        <li v-for="(item,key) in validation" :key="key">{{item[0]}}</li>
+                    </ul>
                 </div>
                 <div class="form-actions">
                     <button type="submit" class="button" name="start">Register</button>
@@ -28,20 +61,35 @@
             </form>
         </div>
 
-        <div class="sign-up-form" v-if="!register" id="login">
+        <div class="sign-up-form" v-if="!register" id="login" @submit.prevent="submitLogin"
+             :class="{'is-loading' : loading}">
             <h3>Login Now!</h3>
             <form>
                 <div class="form-group">
-                    <input type="email" name="email" class="form-control has-custom" placeholder="E-Mail">
+                    <input type="email" class="form-control has-custom"
+                           name="email"
+                           placeholder="E-mail or user name"
+                           v-model="form.email"
+                           required
+                    >
                 </div>
                 <div class="form-group">
-                    <input type="password" name="password" class="form-control has-custom" placeholder="Password">
+                    <input type="password" name="password" class="form-control has-custom"
+                           placeholder="Password" v-model="form.password" required
+                    >
                 </div>
                 <ul class="form-status">
                     <li></li>
                     <li></li>
                     <li></li>
                 </ul>
+                <div class="checking">
+                    <input type="checkbox" id="lchecked" class="check" v-model="form.remember_me">
+                    <label for="lchecked">Remember me next time</label>
+                </div>
+                <div class="alert" :class="'alert-'+message.type" role="alert" v-if="message.show">
+                    {{message.content}}
+                </div>
                 <div class="form-actions">
                     <button type="submit" class="button" name="start">Login</button>
                     <a class="button no-background" @click="register=true" href="#register">Register</a>
@@ -52,11 +100,63 @@
 </template>
 
 <script>
+    import {mapActions} from 'vuex'
+
     export default {
         name: 'AppSignUpForm',
+        computed: {
+            errors() {
+                return null
+            }
+        },
         data() {
             return {
-                register: true
+                message: {
+                    show: false,
+                    content: '',
+                    type: ''
+                },
+                register: true,
+                validation: null,
+                loading: false,
+                done: false,
+                form: {
+                    name: '',
+                    email: '',
+                    password: '',
+                    newsletter: true
+                }
+            }
+        },
+        methods: {
+            ...mapActions({
+                signup: 'auth/SIGNUP',
+                login: 'auth/LOGIN',
+            }),
+            submitLogin() {
+                this.loading = true;
+                this.message.show = false;
+                this.login(this.form).then((data) => {
+                    if (!data) {
+                        this.message.content = "Email or Password incorrect";
+                        this.message.type = 'danger'
+                        this.message.show = true
+                    } else {
+                        this.$toast.success('Successfully logged in')
+                    }
+                }).finally(() => {
+                    this.loading = false;
+                })
+            },
+            submitSignup() {
+                this.loading = true;
+                this.signup(this.form).then(() => {
+                    this.done = true;
+                }).catch((error) => {
+                    this.validation = this.$utils.getGqlValidationErrors(error)
+                }).finally(() => {
+                    this.loading = false;
+                })
             }
         }
     }
--- front/store/auth.js
+++ front/store/auth.js
@@ -4,81 +4,81 @@ import signupQql from '@/graphql/queries/auth/signup.graphql';
 import meQql from '@/graphql/queries/auth/me.graphql';
 
 export const state = () => ({
-  loggedIn: false,
-  me: null,
-  me_token: {
-    access_token: null,
-    expires_at: null
-  }
+    loggedIn: false,
+    me: null,
+    me_token: {
+        access_token: null,
+        expires_at: null
+    }
 })
 
 export const mutations = {
-  //Set the latest news to state
-  SET_ME: (state, val) => {
-    state.me = val
-  },
+    //Set the latest news to state
+    SET_ME: (state, val) => {
+        state.me = val
+    },
 
-  SET_ME_TOKEN: (state, val) => {
-    state.me_token = val
-  },
+    SET_ME_TOKEN: (state, val) => {
+        state.me_token = val
+    },
 
-  SET_LOGGED_IN: (state, val) => {
-    state.loggedIn = val
-  },
+    SET_LOGGED_IN: (state, val) => {
+        state.loggedIn = val
+    },
 }
 
 export const actions = {
-  //Get the latest news
-  async SET_TOKEN({commit, dispatch}, token) {
-    commit('SET_ME_TOKEN', {access_token: token})
-    commit('SET_LOGGED_IN', true)
-    await this.app.$apolloHelpers.onLogin(token)
-    await dispatch('LOAD_ME')
-    console.log('SET_TOKEN')
-    this.$router.push('/')
-    return true;
-  },
-  async LOAD_ME({commit}) {
-    await this.app.apolloProvider.defaultClient.query({query: meQql})
-      .then(({data}) => {
-        console.log(data)
-        if (data.me) {
-          commit('SET_ME', data.me)
-          commit('SET_LOGGED_IN', true)
-        }
-      }).catch((error) => {
-        console.log(error)
-      })
-  },
-  async LOGIN({commit}, params) {
-    return await this.app.apolloProvider.defaultClient.query({query: loginQql, variables: params})
-      .then(async ({data}) => {
-        if (data.login) {
-          await this.app.$apolloHelpers.onLogin(data.login.access_token)
-          commit('SET_ME_TOKEN', data.login)
-          commit('SET_LOGGED_IN', true)
-          this.$router.push('/')
-          return true
-        }
-        return false
-      })
-  },
-  async LOGOUT({commit}) {
-    await this.app.apolloProvider.defaultClient.query({query: logoutQql})
-      .then(async ({data}) => {
-        commit('SET_LOGGED_IN', false)
-        await this.app.$apolloHelpers.onLogout()
-        this.$toast.success(data.logout)
+    //Get the latest news
+    async SET_TOKEN({commit, dispatch}, token) {
+        commit('SET_ME_TOKEN', {access_token: token})
+        commit('SET_LOGGED_IN', true)
+        await this.app.$apolloHelpers.onLogin(token)
+        await dispatch('LOAD_ME')
+        console.log('SET_TOKEN')
         this.$router.push('/')
-      }).catch((error) => {
-        console.log(error)
-      })
-  },
-  async SIGNUP({commit}, params) {
-    await this.app.apolloProvider.defaultClient.mutate({mutation: signupQql, variables: params})
-      .then(({data}) => {
-        if (data.signup)
-          this.$router.push('/auth/login')
-      })
-  },
+        return true;
+    },
+    async LOAD_ME({commit}) {
+        await this.app.apolloProvider.defaultClient.query({query: meQql})
+            .then(({data}) => {
+                console.log(data)
+                if (data.me) {
+                    commit('SET_ME', data.me)
+                    commit('SET_LOGGED_IN', true)
+                }
+            }).catch((error) => {
+                console.log(error)
+            })
+    },
+    async LOGIN({commit}, params) {
+        return await this.app.apolloProvider.defaultClient.query({query: loginQql, variables: params})
+            .then(async ({data}) => {
+                if (data.login) {
+                    await this.app.$apolloHelpers.onLogin(data.login.access_token)
+                    commit('SET_ME_TOKEN', data.login)
+                    commit('SET_LOGGED_IN', true)
+                    this.$router.push('/auth/account')
+                    return true
+                }
+                return false
+            })
+    },
+    async LOGOUT({commit}) {
+        await this.app.apolloProvider.defaultClient.query({query: logoutQql})
+            .then(async ({data}) => {
+                commit('SET_LOGGED_IN', false)
+                await this.app.$apolloHelpers.onLogout()
+                this.$toast.success(data.logout)
+                this.$router.push('/')
+            }).catch((error) => {
+                console.log(error)
+            })
+    },
+    async SIGNUP({commit}, params) {
+        await this.app.apolloProvider.defaultClient.mutate({mutation: signupQql, variables: params})
+            .then(({data}) => {
+                if (data.signup)
+                    this.$router.push('/auth/login')
+            })
+    },
 }
