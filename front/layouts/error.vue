2f60ce738934294957cc37a6ce6f277482686e9e
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-03-18 16:03:56 +0100

    display error

--- front/layouts/error.vue
+++ front/layouts/error.vue
@@ -4,6 +4,7 @@
             <img src="@/assets/images/404.png" alt="LV">
         </div>
         <div class="error-body">
+            <pre>{{error}}</pre>
             <h1 v-if="error.statusCode === 404">Mate, page not found!</h1>
             <h1 v-else>An error occurred</h1>
             <p>You might not have permissions to see this page or isn’t exists anymore.</p>
@@ -16,33 +17,38 @@
 </template>
 
 <script>
-  export default {
-    props: ['error'],
-    layout: ({store}) => store.state.platform
-  }
+    export default {
+        props: ['error'],
+        layout: ({store}) => store.state.platform
+    }
 </script>
 
 <style lang="stylus" scoped>
     .error-page
         padding 120px 0
+
         .error-thumb
             text-align center
             margin-bottom 60px
+
         .error-body
             text-align center
             width 460px
             margin auto
+
             h1
                 color $tertiary
                 font-size 28px
                 font-weight 600
                 margin-bottom 10px
                 line-height 1.4
+
             p
                 font-size 14px
                 color #616d82
                 opacity .8
                 margin-bottom 40px
+
             .button
                 width 280px
                 margin auto
