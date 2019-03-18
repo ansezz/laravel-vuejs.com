a4073e2cccfeccba7f6f2b49f193e4e071769e5b
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2019-03-18 16:03:49 +0100

    create dmca policy page

---
+++ front/pages/page/dmca-policy.vue
@@ -0,0 +1,30 @@
+<template>
+    <component :is="this.$store.state.platform"/>
+</template>
+
+<script>
+    import seo from '@/mixins/seo'
+
+    export default {
+        mixins: [seo],
+        layout: ({store}) => store.state.platform,
+        computed: {
+            seo() {
+                return {
+                    title: '🚀 Dmca Policy ',
+                    description: 'Dmca Policy page',
+                    image: '',
+                    url: 'https://laravel-vuejs.com/dmca-policy',
+                    type: 'website',
+                }
+            },
+        },
+        components: {
+            mobile: () => import(`~/views/mobile/page/dmca-policy`),
+            web: () => import(`~/views/web/page/dmca-policy`)
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ front/views/mobile/page/dmca-policy.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+        <div class="container">
+            <h1>dmca-policy Mobile</h1>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        components: {},
+        name: "dmca-policy"
+    }
+</script>
+
+<style lang="stylus" scoped>
+</style>
