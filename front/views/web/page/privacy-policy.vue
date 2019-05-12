6973ed59379c18927818641ea2b8dda6c80059e7
Othman <othmandeveloppeur@gmail.com>
2019-05-12 14:43:50 +0000

    privacy policy web

--- front/views/web/page/privacy-policy.vue
+++ front/views/web/page/privacy-policy.vue
@@ -148,7 +148,7 @@
 
 <style lang="stylus" scoped>
     .dmca-container
-        padding-bottom 120px
+        padding-bottom 30px
     header
         text-align center
         margin 60px 0 30px
@@ -158,4 +158,10 @@
             color $tertiary
             margin 10px 0
             line-height 1.4
+    .terms-desc
+      p
+        font-size 18px
+        line-height 28px
+        color #616d82
+        margin-bottom 20px
 </style>
