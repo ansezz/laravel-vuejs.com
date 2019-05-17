656d4ac7471e412970bff6d15d00603f04a3e35f
Omar Bourhaouta <bourhaouta@gmail.com>
2019-05-17 14:41:05 +0000

    Update navbar component

--- front/layouts/web.vue
+++ front/layouts/web.vue
@@ -1,53 +1,52 @@
 <template>
-    <div class="app">
-        <app-navbar/>
-        <app-navigation/>
-        <app-main>
-            <nuxt :nuxt-child-key="$route.fullPath"/>
-        </app-main>
-        <div class="container">
-            <adsbygoogle/>
-        </div>
-        <app-subscribe/>
-        <app-footer/>
+  <div class="app">
+    <Navbar/>
+    <app-navigation/>
+    <app-main>
+      <nuxt :nuxt-child-key="$route.fullPath"/>
+    </app-main>
+    <div class="container">
+      <adsbygoogle/>
     </div>
+    <app-subscribe/>
+    <app-footer/>
+  </div>
 </template>
 
 <script>
-    import AppNavbar from "@/components/web/partials/app-navbar"
-    import AppFooter from "@/components/web/partials/app-footer"
-    import AppNavigation from "@/components/web/partials/app-navigation"
-    import AppMain from "@/components/web/partials/app-main"
-    import AppSubscribe from "@/components/web/partials/app-subscribe"
-    import {mapActions} from 'vuex'
+  import AppNavbar from "@/components/web/partials/app-navbar";
+  import AppFooter from "@/components/web/partials/app-footer";
+  import AppNavigation from "@/components/web/partials/app-navigation";
+  import AppMain from "@/components/web/partials/app-main";
+  import AppSubscribe from "@/components/web/partials/app-subscribe";
+  import { mapActions } from "vuex";
 
-    export default {
-        components: {
-            AppNavbar,
-            AppFooter,
-            AppNavigation,
-            AppMain,
-            AppSubscribe
-        },
-        methods: {
-            ...mapActions({
-                setToken: 'auth/SET_TOKEN',
-            }),
-        },
-        data() {
-            return {}
-        },
-        created() {
-            if (this.$route.query.token) {
-                console.log(this.$route.query.token)
-                this.setToken(this.$route.query.token)
-            }
-        }
+  export default {
+    components: {
+      Navbar: () => import("@/components/web/organisms/navbar"),
+      AppFooter,
+      AppNavigation,
+      AppMain,
+      AppSubscribe
+    },
+    methods: {
+      ...mapActions({
+        setToken: "auth/SET_TOKEN"
+      })
+    },
+    data() {
+      return {};
+    },
+    created() {
+      if (this.$route.query.token) {
+        console.log(this.$route.query.token);
+        this.setToken(this.$route.query.token);
+      }
     }
-
+  };
 </script>
 
 <style lang="stylus">
-    .app
-        min-height 100vh
+  .app
+    min-height 100vh
 </style>
