6c64560a7dde194fa0645b66e0c2dbe2b6485870
ANASS EZ-ZOUAINE <ans-ezzouaine@hotmail.com>
2019-05-22 23:41:43 +0000

    beta version

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -96,7 +96,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s  ←  Laravel-VueJs.com',
+    titleTemplate: '%s  ←  Laravel-VueJs.com > Beta',
     title: "Laravel-VueJs.com",
     meta: [
       {charset: "utf-8"}, {name: "viewport", content: "width=device-width, initial-scale=1"}
