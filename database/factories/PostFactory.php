d16d35fdf1c98087bab8e7d9468fb3830dc4b1f1
Ansezz <ans-ezzouaine@hotmail.com>
2019-07-14 00:49:48 +0100

    seed content post

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,7 +6,7 @@ $factory->define(\LaravelVueJs\Models\Post::class, function (Faker $faker) {
     return [
         'title'          => $faker->text(200),
         'excerpt'        => $faker->text(100),
-        'content'        => $faker->randomHtml(),
+        'content'        => $faker->text(2000),
         'type'           => 1,
         'status'         => $faker->numberBetween(0, 3),
         'comment_status' => $faker->numberBetween(0, 1),
