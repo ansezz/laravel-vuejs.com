9304faa1956d1e22e5efde73ca7b2b4a0358df9f
Ansezz <ans-ezzouaine@hotmail.com>
2019-07-25 11:42:23 +0100

    fix seo

--- front/mixins/seo.js
+++ front/mixins/seo.js
@@ -3,6 +3,7 @@ let config = {
   twitter: '@LaravelVueJs',
   fbAppId: '111025849614074',
   themeColor: '#42b883',
+  local: 'en',
 }
 
 // seo.url
@@ -37,32 +38,32 @@ const seo = {
         {hid: 'description', name: 'description', content: this.seo.description},
         // OpenGraph data (Most widely used)
 
-        {name: 'fb:app_id', content: config.fbAppId},
+        {hid: 'fb:app_id', property: 'fb:app_id', content: config.fbAppId},
         {name: 'author', content: this.seo.url},
         {name: 'publisher', content: this.seo.url},
         {hid: 'apple-mobile-web-app-title', name: 'apple-mobile-web-app-title', content: this.seo.title},
         {name: 'theme-color', content: config.themeColor},
 
         // The list of types is available here: http://ogp.me/#types
-        {hid: 'og:type',name: 'og:type', content: this.seo.type},
+        {hid: 'og:type', property: 'og:type', content: this.seo.type},
         // Should the the same as your canonical link, see below.
-        {hid: 'og:url',name: 'og:url', content: this.seo.url},
-        {hid: 'og:image',name: 'og:image', content: this.seo.image},
+        {hid: 'og:url', property: 'og:url', content: this.seo.url},
+        {hid: 'og:image', property: 'og:image', content: this.seo.image},
         // Often the same as your meta description, but not always.
-        {hid: 'og:description',name: 'og:description', content: this.seo.description},
-        {hid: 'og:title',name: 'og:title', content: this.seo.title},
-        {name: 'og:site_name', content: config.siteName},
-        {name: 'og:locale', content: this.seo.locale},
+        {hid: 'og:description', property: 'og:description', content: this.seo.description},
+        {hid: 'og:title', property: 'og:title', content: this.seo.title},
+        {hid: 'og:site_name', property: 'og:site_name', content: config.siteName},
+        {hid: 'og:locale', property: 'og:locale', content: config.locale},
 
         // Twitter card
-        {hid: 'twitter:card',name: 'twitter:card', content: 'summary_large_image'},
-        {hid: 'twitter:site',name: 'twitter:site', content: this.seo.url},
-        {hid: 'twitter:title',name: 'twitter:title', content: this.seo.title},
-        {hid: 'twitter:description',name: 'twitter:description', content: this.seo.description},
+        {hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image'},
+        {hid: 'twitter:site', name: 'twitter:site', content: this.seo.url},
+        {hid: 'twitter:title', name: 'twitter:title', content: this.seo.title},
+        {hid: 'twitter:description', name: 'twitter:description', content: this.seo.description},
         // Your twitter handle, if you have one.
-        {hid: 'twitter:creator',name: 'twitter:creator', content: config.twitter},
-        {hid: 'twitter:image',name: 'twitter:image', content: this.seo.image},
-        {hid: 'twitter:image:src',name: 'twitter:image:src', content: this.seo.image},
+        {hid: 'twitter:creator', name: 'twitter:creator', content: config.twitter},
+        {hid: 'twitter:image', name: 'twitter:image', content: this.seo.image},
+        {hid: 'twitter:image:src', name: 'twitter:image:src', content: this.seo.image},
 
         // Google / Schema.org markup:
         {itemprop: 'name', content: this.seo.title},
